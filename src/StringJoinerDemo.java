8888eb399df46b23bfb35464f39fa6363ba3f878
rakshitha2708 <124131015+rakshitha2708@users.noreply.github.com>
2024-09-01 14:21:28 +0530

    firstsep

---
+++ src/StringJoinerDemo.java
@@ -0,0 +1,12 @@
+import java.util.StringJoiner;
+
+public class StringJoinerDemo {
+    public static void main(String[] args) {
+        StringJoiner joinnames= new StringJoiner(",");
+        joinnames.add("Raksha");
+        joinnames.add("Sharu");
+        joinnames.add("sharurakshu");
+        joinnames.add("SR");
+        System.out.println(joinnames);
+    }
+}
